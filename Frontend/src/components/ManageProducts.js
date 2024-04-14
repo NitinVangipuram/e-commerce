4ee8d3daa7228c54c1e93b4952e700da47fc6e85
NitinVangipuram <nitin.vangipuram@gmail.com>
2024-04-14 14:44:07 +0530

    changes

--- Frontend/src/components/ManageProducts.js
+++ Frontend/src/components/ManageProducts.js
@@ -10,11 +10,6 @@ function ManageProducts() {
       .catch(error => console.error("There was an error!", error));
   }, []);
 
-  const handleEdit = (productId) => {
-    console.log('Editing product', productId);
-    // Implementation would go here
-  };
-
   const handleDelete = (productId) => {
     fetch(`http://localhost:5000/api/products/${productId}`, {
       method: 'DELETE',
@@ -28,29 +23,33 @@ function ManageProducts() {
   };
 
   return (
-    <div className="container mx-auto mt-10 px-4">
-      <h1 className="text-2xl font-semibold mb-6">Manage Products</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {products.map(product => (
-          <div key={product._id} className="border rounded-lg overflow-hidden shadow-lg">
-          <div className="h-56 flex justify-center items-center overflow-hidden">
+    <div className="px-4 py-6">
+      <h1 className="text-2xl font-bold mb-5">Manage Products</h1>
+      {products.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map(product => (
+            <div key={product._id} className="flex flex-col border rounded-lg overflow-hidden shadow-lg">
+            <div className="h-56 flex justify-center items-center overflow-hidden">
         <img src={`http://localhost:5000/${product.images[0]}`} alt={product.name} className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110" />
       </div>
-            <div className="p-4">
-              <h5 className="text-lg font-bold">{product.name}</h5>
-              <p className="text-gray-600 text-sm my-2">{product.description}</p>
-              <div className="flex justify-between mt-3">
-                {/* <button onClick={() => handleEdit(product._id)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors duration-200">
-                  Edit
-                </button> */}
-                <button onClick={() => handleDelete(product._id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition-colors duration-200">
-                  Delete
-                </button>
+              <div className="p-4 flex flex-1 flex-col">
+                <h5 className="text-lg font-bold">{product.name}</h5>
+                <p className="text-gray-600 text-sm flex-1">{product.aboutThisItem[0].substring(0, 150)}...</p>
+                <div className="flex justify-end mt-3">
+                  <button
+                    onClick={() => handleDelete(product._id)}
+                    className="py-2 px-4 bg-red-500 hover:bg-red-700 text-white font-bold rounded transition duration-300"
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-xl text-gray-600">No products available.</p>
+      )}
     </div>
   );
 }
--- Frontend/src/components/Products.js
+++ Frontend/src/components/Products.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 function Products() {
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
+    const [categories, setCategories] = useState(['All']);
     const [activeCategory, setActiveCategory] = useState('All');
 
     useEffect(() => {
-        // Fetch products from the backend
         fetch('http://localhost:5000/api/products')
             .then(response => response.json())
             .then(data => {
                 setProducts(data);
                 setFilteredProducts(data);
+                // Extract unique categories
+                const uniqueCategories = ['All', ...new Set(data.map(product => product.category))];
+                setCategories(uniqueCategories);
             })
             .catch(error => console.error("There was an error!", error));
     }, []);
 
-    const categories = ['All', 'Men', 'Women', 'Kids', 'Sports'];
-    const truncateText = (text, length) => {
-        if (text.length <= length) return text;
-        return text.substring(0, length) + '...';
-    };
     const filterProducts = (category) => {
         setActiveCategory(category);
         if (category === 'All') {
@@ -71,108 +69,88 @@ function Products() {
             },
         ],
     };
-console.log(products);
-function calculateStars(averageRating) {
-    let starOutput = '';
-    const fullStars = Math.floor(averageRating);
-    const halfStar = averageRating % 1 >= 0.5 ? 1 : 0;
-    const emptyStars = 5 - fullStars - halfStar;
-  
-    starOutput += '★'.repeat(fullStars); // Add full stars
-    if (halfStar) {
-      starOutput += '½'; // Add half star. Make sure you use a font or method to display this properly, or use an alternative half star character/emoji/image.
+
+    function calculateStars(averageRating) {
+        let starOutput = '';
+        const fullStars = Math.floor(averageRating);
+        const halfStar = averageRating % 1 >= 0.5 ? 1 : 0;
+        const emptyStars = 5 - fullStars - halfStar;
+      
+        starOutput += '★'.repeat(fullStars);
+        if (halfStar) {
+          starOutput += '½';
+        }
+        starOutput += '☆'.repeat(emptyStars);
+      
+        return starOutput;
     }
-    starOutput += '☆'.repeat(emptyStars); // Add empty stars
-  
-    return starOutput;
-  }
+
     return (
         <div className="py-4 bg-gray-100 min-h-screen">
             <div className="container mx-auto px-4">
                 <h2 className="text-3xl font-semibold mb-8 text-center">Featured Products</h2>
                 
                 <Slider {...sliderSettings}>
-                
-  {products.map((product) => (
-    <div key={product._id} className="px-2">
-  <Link to={`/product/${product._id}`}>
-    <div className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow transition-shadow duration-300 ease-in-out flex flex-col hover:shadow-lg transform hover:-translate-y-1 hover:scale-105">
-      <div className="h-56 flex justify-center items-center overflow-hidden">
-        <img src={`http://localhost:5000/${product.images[0]}`} alt={product.name} className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110" />
-      </div>
-      <div className="p-4 flex flex-col flex-1">
-        {/* Optional: Display category or tag */}
-        <span className="text-sm text-gray-500 mb-1">{
-  product.category ?
-    <div>{product.category}</div> : // What to render if product.category is truthy
-    <div>Category</div>             // What to render if product.category is falsy
-}
-</span>
-        <h3 className="text-lg font-semibold mb-2 line-clamp-1">{product.name}</h3> {/* Ensures text does not overflow */}
-        {/* Optional: Display ratings if available */}
-        <div className="flex items-center mb-2">
-          <span className="text-yellow-400 text-xs">{calculateStars(product.averageRating)}</span>
-          <span className="text-gray-500 text-xs ml-2">({product.ratings.length} reviews)</span>
-        </div>
-        <p className="text-gray-800 font-medium mt-auto">₹{product.price}</p>
-      </div>
-    </div>
-  </Link>
-</div>
-
-  ))}
-
-
+                    {products.map((product) => (
+                        <div key={product._id} className="px-2">
+                            <Link to={`/product/${product._id}`}>
+                                <div className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow transition-shadow duration-300 ease-in-out flex flex-col hover:shadow-lg transform hover:-translate-y-1 hover:scale-105">
+                                    <div className="h-56 flex justify-center items-center overflow-hidden">
+                                        <img src={`http://localhost:5000/${product.images[0]}`} alt={product.name} className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110" />
+                                    </div>
+                                    <div className="p-4 flex flex-col flex-1">
+                                        <span className="text-sm text-gray-500 mb-1">{product.category || "Category"}</span>
+                                        <h3 className="text-lg font-semibold mb-2 line-clamp-1">{product.name}</h3>
+                                        <div className="flex items-center mb-2">
+                                            <span className="text-yellow-400 text-xs">{calculateStars(product.averageRating)}</span>
+                                            <span className="text-gray-500 text-xs ml-2">({product.ratings.length} reviews)</span>
+                                        </div>
+                                        <p className="text-gray-800 font-medium mt-auto">₹{product.price}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
                 </Slider>
 
                 <div className="my-12">
-    <h2 className="text-3xl font-semibold mb-6 text-center">Shop by Category</h2>
-    <div className="flex justify-center flex-wrap gap-6">
-        {categories.map((category) => (
-            <button
-                key={category}
-                className={`py-3 px-8 rounded-full text-sm font-medium transition-all duration-150 ease-in-out transform hover:scale-105 ${
-                    activeCategory === category
-                    ? 'bg-gray-800 text-white shadow-lg' // Dark color for active category
-                    : 'bg-white text-gray-800 hover:bg-gray-100 shadow-md border border-gray-200'
-                }`}
-                onClick={() => filterProducts(category)}
-            >
-                {category}
-            </button>
-        ))}
-    </div>
-</div>
-
+                    <h2 className="text-3xl font-semibold mb-6 text-center">Shop by Category</h2>
+                    <div className="flex justify-center flex-wrap gap-6">
+                        {categories.map((category) => (
+                            <button
+                                key={category}
+                                className={`py-3 px-8 rounded-full text-sm font-medium transition-all duration-150 ease-in-out transform hover:scale-105 ${
+                                    activeCategory === category ? 'bg-gray-800 text-white shadow-lg' : 'bg-white text-gray-800 hover:bg-gray-100 shadow-md border border-gray-200'
+                                }`}
+                                onClick={() => filterProducts(category)}
+                            >
+                                {category}
+                            </button>
+                        ))}
+                    </div>
+                </div>
 
                 <h2 className="text-3xl font-semibold mb-6 text-center">Our Products</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {filteredProducts.map((product) => (
                         <div key={product._id} className="px-2">
-  <Link to={`/product/${product._id}`}>
-    <div className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow transition-shadow duration-300 ease-in-out flex flex-col hover:shadow-lg transform hover:-translate-y-1 hover:scale-105">
-      <div className="h-56 flex justify-center items-center overflow-hidden">
-        <img src={`http://localhost:5000/${product.images[0]}`} alt={product.name} className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110" />
-      </div>
-      <div className="p-4 flex flex-col flex-1">
-        {/* Optional: Display category or tag */}
-        <span className="text-sm text-gray-500 mb-1">{
-  product.category ?
-    <div>{product.category}</div> : // What to render if product.category is truthy
-    <div>Category</div>             // What to render if product.category is falsy
-}
-</span>
-        <h3 className="text-lg font-semibold mb-2 line-clamp-1">{product.name}</h3> {/* Ensures text does not overflow */}
-        {/* Optional: Display ratings if available */}
-        <div className="flex items-center mb-2">
-          <span className="text-yellow-400 text-xs">{calculateStars(product.averageRating)}</span>
-          <span className="text-gray-500 text-xs ml-2">({product.ratings.length} reviews)</span>
-        </div>
-        <p className="text-gray-800 font-medium mt-auto">₹{product.price}</p>
-      </div>
-    </div>
-  </Link>
-</div>
+                            <Link to={`/product/${product._id}`}>
+                                <div className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow transition-shadow duration-300 ease-in-out flex flex-col hover:shadow-lg transform hover:-translate-y-1 hover:scale-105">
+                                    <div className="h-56 flex justify-center items-center overflow-hidden">
+                                        <img src={`http://localhost:5000/${product.images[0]}`} alt={product.name} className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110" />
+                                    </div>
+                                    <div className="p-4 flex flex-col flex-1">
+                                        <span className="text-sm text-gray-500 mb-1">{product.category || "Category"}</span>
+                                        <h3 className="text-lg font-semibold mb-2 line-clamp-1">{product.name}</h3>
+                                        <div className="flex items-center mb-2">
+                                            <span className="text-yellow-400 text-xs">{calculateStars(product.averageRating)}</span>
+                                            <span className="text-gray-500 text-xs ml-2">({product.ratings.length} reviews)</span>
+                                        </div>
+                                        <p className="text-gray-800 font-medium mt-auto">₹{product.price}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
                     ))}
                 </div>
             </div>
